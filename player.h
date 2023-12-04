b6e559f4c6037a0ac19bbfdf825d6083652728f9
Lucy Gavrich <annalgav@gmail.com>
2023-12-03 19:59:42 -0800

    Rename playerType.h to player.h to fit naming conventions. Rename variables and methods to fit naming conventions. Switch from cumulative tracker string to process-of-elimination tracker string to adapt to new decision tree form.

---
+++ player.h
@@ -0,0 +1,20 @@
+#ifndef PROJECT_PLAYER_H_
+#define PROJECT_PLAYER_H_
+
+#include <string>
+
+class Player {
+public:
+  Player(std::string = "");
+  ~Player(){};
+  void SetName(std::string);
+  std::string GetName() const;
+  void RemoveTrait(char);
+  char GetFinalTrait() const;
+
+private:
+  std::string name_;
+  std::string tracker_;
+};
+
+#endif
