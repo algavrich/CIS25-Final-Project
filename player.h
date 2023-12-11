afd6a41b374b6d62e9a3e29ba4f3c1403da9a3be
Lucy Gavrich <annalgav@gmail.com>
2023-12-11 12:58:04 -0800

    Add comments and adjust indentation level from 2 to 4 spaces for readability

--- player.h
+++ player.h
@@ -1,3 +1,5 @@
+// Player of the game
+
 #ifndef PROJECT_PLAYER_H_
 #define PROJECT_PLAYER_H_
 
@@ -5,16 +7,16 @@
 
 class Player {
 public:
-  Player(std::string = "");
-  ~Player(){};
-  void SetName(std::string);
-  std::string GetName() const;
-  void RemoveTrait(char);
-  char GetFinalTrait() const;
+    Player(std::string = "");
+    ~Player(){};
+    void SetName(std::string);
+    std::string GetName() const;
+    void RemoveTrait(char);
+    char GetFinalTrait() const;
 
 private:
-  std::string name_;
-  std::string tracker_;
+    std::string name_;
+    std::string tracker_;
 };
 
 #endif
--- player_imp.cpp
+++ player_imp.cpp
@@ -1,3 +1,5 @@
+// Implementation file for Player type
+
 #include "player.h"
 
 #include <iostream>
@@ -6,22 +8,33 @@
 using namespace std;
 
 Player::Player(string name) {
-  name_ = name;
-  tracker_ = "ABCD";
+    // Constructor
+        // Initialize name to value of argument and tracker to "ABCD"
+
+    name_ = name;
+    tracker_ = "ABCD";
 }
 
 void Player::SetName(string name) {
-  name_ = name;
+
+    name_ = name;
 }
 
 string Player::GetName() const {
-  return name_;
+
+    return name_;
 }
 
 void Player::RemoveTrait(char trait) {
-  tracker_.erase(tracker_.find(trait), 1);
+    // Remove a trait
+        // Utilize 'erase' and 'find' string methods to locate and erase a
+        //trait character from tracker_
+
+    tracker_.erase(tracker_.find(trait), 1);
 }
 
 char Player::GetFinalTrait() const {
-  return static_cast<char>(tracker_[0]);
+    // Return the remaining trait character in tracker_
+
+    return static_cast<char>(tracker_[0]);
 }
